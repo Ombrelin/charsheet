2d83571d8511acf239d6a7c8af4c2656f51e3669
Arsène Lapostolet <arsene.lapostolet@edifixio.com>
2019-12-04 20:35:14 +0100

    added life bar, mana bar, characteristics bars

--- src/main/java/fr/arsene/charsheet/controller/MainController.java
+++ src/main/java/fr/arsene/charsheet/controller/MainController.java
@@ -1,9 +1,20 @@
 package fr.arsene.charsheet.controller;
 
+import com.jfoenix.controls.JFXProgressBar;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
 @FxmlView("main.fxml")
-public class MainController {
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
