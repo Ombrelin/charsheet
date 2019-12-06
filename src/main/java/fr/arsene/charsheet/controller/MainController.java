f89dc585cd3e7fe29ceb6c6bc8fc4fac9bafe1f3
Arsène Lapostolet <arsene.lapostolet@edifixio.com>
2019-12-06 20:51:35 +0100

    equipement lists

--- src/main/java/fr/arsene/charsheet/controller/MainController.java
+++ src/main/java/fr/arsene/charsheet/controller/MainController.java
@@ -1,20 +1,25 @@
 package fr.arsene.charsheet.controller;
 
-import com.jfoenix.controls.JFXProgressBar;
+import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 @Component
-@FxmlView("main.fxml")
-public class MainController implements Initializable {
+@FxmlView
+public class MainController {
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    private JFXComboBox<Label> comboboxGender;
 
+
+    @FXML
+    public void initialize() {
+        System.out.println("test");
+        this.comboboxGender.getItems().addAll(new Label("Masculin"), new Label("Féminin"));
+        this.comboboxGender.setFocusColor(new Color(50f/255f,50/255f,50/255f,1));
     }
 }
