3debc3374d65c0c7c46ea19c37f2d6ea38ece19b
Arsène Lapostolet <arsene@lapostolet.fr>
2019-12-07 20:36:44 +0100

    started to user sub components

--- src/main/java/fr/arsene/charsheet/components/CharacteristicBar.java
+++ src/main/java/fr/arsene/charsheet/components/CharacteristicBar.java
@@ -1,19 +1,43 @@
 package fr.arsene.charsheet.components;
 
-import fr.arsene.charsheet.controller.CharacteristicBarController;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import net.rgielen.fxweaver.core.FxWeaver;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
 
+import java.io.IOException;
 
-public class CharacteristicBar extends Group {
+public class CharacteristicBar extends FlowPane {
 
-    @Autowired
-    FxWeaver fxWeaver;
+    @FXML
+    private JFXTextField field;
+
+    private SimpleFloatProperty value = new SimpleFloatProperty();
 
     public CharacteristicBar() {
-        Node n = fxWeaver.loadView(CharacteristicBarController.class);
-        this.getChildren().add(n);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(
+                "CharacteristicBar.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    private void handleInput(KeyEvent event) {
+        value.set(Float.parseFloat(field.getText()) / 20f);
+        System.out.println(field.getText());
+    }
+
+    public float getValue() {
+        float val = value.get() * 20;
+        System.out.println(val);
+        return val;
     }
 }
--- src/main/java/fr/arsene/charsheet/controller/MainController.java
+++ src/main/java/fr/arsene/charsheet/controller/MainController.java
@@ -1,15 +1,17 @@
 package fr.arsene.charsheet.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXProgressBar;
 import com.jfoenix.controls.JFXTextField;
+import fr.arsene.charsheet.components.CharacteristicBar;
 import fr.arsene.charsheet.model.character.Gender;
 import fr.arsene.charsheet.model.game.GameModel;
 import fr.arsene.charsheet.services.CharacterService;
 import fr.arsene.charsheet.services.GameModelService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,67 +46,15 @@ public class MainController {
     private JFXComboBox<Label> comboboxProfession;
 
     @FXML
-    private CharacteristicBarController forceCharBar;
+    private JFXButton saveButton;
 
     // Characteristics
 
-      /*
-
     @FXML
-    private JFXTextField forceField;
-
-    @FXML
-    private JFXProgressBar forceBar;
-
-    @FXML
-    private JFXTextField intelligenceField;
-
-    @FXML
-    private JFXProgressBar intelligenceBar;
+    private CharacteristicBar forceCharBar;
 
 
-    @FXML
-    private JFXTextField courageField;
 
-    @FXML
-    private JFXProgressBar courageBar;
-
-    @FXML
-    private JFXTextField charismaField;
-
-    @FXML
-    private JFXProgressBar charismaBar;
-
-    @FXML
-    private JFXTextField agilityField;
-
-    @FXML
-    private JFXProgressBar agilityBar;
-
-    @FXML
-    private JFXTextField attackField;
-
-    @FXML
-    private JFXProgressBar attackBar;
-
-    @FXML
-    private JFXTextField blockField;
-
-    @FXML
-    private JFXProgressBar blockBar;
-
-    // Properties
-
-
-    private SimpleFloatProperty forceProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty intelligenceProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty courageProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty charismaProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty agilityProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty attackProperty = new SimpleFloatProperty();
-    private SimpleFloatProperty blockProperty = new SimpleFloatProperty();
-
-     */
 
     // Control maps
 
@@ -115,35 +65,15 @@ public class MainController {
         this.comboboxGender.getItems().addAll(Arrays.stream(Gender.values()).map(gender -> new Label(gender.toString())).collect(Collectors.toList()));
         this.comboboxGender.setFocusColor(new Color(50f / 255f, 50 / 255f, 50 / 255f, 1));
 
-        /*
-        forceBar.progressProperty().bind(forceProperty);
-        intelligenceBar.progressProperty().bind(intelligenceProperty);
-        courageBar.progressProperty().bind(courageProperty);
-        charismaBar.progressProperty().bind(charismaProperty);
-        agilityBar.progressProperty().bind(agilityProperty);
-        attackBar.progressProperty().bind(attackProperty);
-        blockBar.progressProperty().bind(blockProperty);
-         fieldBarMap.put(forceField, forceBar);
-
-        */
-
-
-
 
         GameModel model = gameModelService.getGameModel();
         this.comboboxRace.getItems().addAll(model.getRaces().stream().map(race -> new Label(race.getName())).collect(Collectors.toList()));
         this.comboboxProfession.getItems().addAll(model.getProfessions().stream().map(profession -> new Label(profession.getName())).collect(Collectors.toList()));
     }
 
-
-    private void handleChangeCharacteristic(KeyEvent event) {
-        /*
-        try {
-            this.courageProperty.set(Integer.parseInt(field.getText()) / 20f);
-        } catch (NumberFormatException e) {
-            field.setText("0");
-        }
-
-         */
+    @FXML
+    private void handleClickSave(ActionEvent event){
+        System.out.println(forceCharBar.getValue());
     }
+
 }
